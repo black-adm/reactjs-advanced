a8463c225d9bb8bfb8af0d8dd07fbe8b0d5b204e
black-adm <mattheuscontato17@gmail.com>
2024-02-22 20:17:25 -0300

    header component

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,22 @@
+import { Plus } from 'lucide-react'
+import { Header } from './components/header'
+
 export function App() {
-  return <h1 className="text-4xl font-medium">Olá mundo</h1>
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        <Header />
+      </div>
+
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className="flex items-center gap-3">
+          <h1 className="text-xl uppercase tracking-wide">tags</h1>
+          <button className="inline-flex items-center gap-1.5 text-xs bg-lime-300 text-slate-900 font-medium rounded-full px-3 py-1">
+            <Plus size={10} strokeWidth={3} />
+            Criar nova
+          </button>
+        </div>
+      </main>
+    </div>
+  )
 }
