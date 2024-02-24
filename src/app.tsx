35079eb2e7511f6d55588bec50f4664060f45663
black-adm <mattheuscontato17@gmail.com>
2024-02-23 22:15:01 -0300

    fix: fake data json returned in tables

--- src/app.tsx
+++ src/app.tsx
@@ -12,8 +12,39 @@ import {
   TableRow,
 } from './components/table'
 import { Pagination } from './components/pagination'
+import { useQuery } from '@tanstack/react-query'
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  id: string
+  title: string
+  company: string
+  period: string
+}
 
 export function App() {
+  const baseUrl = 'http://localhost:3333'
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryKey: ['get-tags'],
+    queryFn: async () => {
+      const request = await fetch(`${baseUrl}/tags?_page=1&_per_page=10`)
+      const response = request.json()
+      return response
+    },
+  })
+
+  if (isLoading) return null
+
   return (
     <div className="py-10 space-y-8">
       <div>
@@ -53,24 +84,24 @@ export function App() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Array.from({ length: 7 }).map((value, index) => {
+            {tagsResponse?.data.map((tag) => {
               return (
-                <TableRow key={index}>
+                <TableRow key={tag.id}>
                   <TableCell></TableCell>
                   <TableCell>
                     <div className="flex flex-col gap-0.5">
-                      <span className="font-medium">Fluig</span>
+                      <span className="font-medium">{tag.title}</span>
                       <span className="text-xs text-slate-600">
                         1aefccd5-3b96-4de5-b07b-6109e7e53997
                       </span>
                     </div>
                   </TableCell>
 
-                  <TableCell className="text-zinc-300 text-xs">
-                    Mackenzie
+                  <TableCell className="text-slate-300 text-xs">
+                    {tag.company}
                   </TableCell>
-                  <TableCell className="text-zinc-300 tex-xs">
-                    3 mese(s)
+                  <TableCell className="text-slate-300 tex-xs">
+                    {tag.period}
                   </TableCell>
                   <TableCell className="text-right">
                     <Button size="icon">
@@ -82,8 +113,9 @@ export function App() {
             })}
           </TableBody>
         </Table>
-        <Pagination />
+        {/* <Pagination /> */}
       </main>
     </div>
   )
+  f
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { App } from './app'
+
 import './globals.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
